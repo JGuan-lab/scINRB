ed6ba539cdc9a721b3c3e5dbcff9df7f94a7ee3a
JGuan-lab <64457671+JGuan-lab@users.noreply.github.com>
2023-01-03 23:06:04 +0800

    Update run_cluster.R

--- code/run_cluster.R
+++ code/run_cluster.R
@@ -7,7 +7,7 @@ library(fpc)
 library(mclust)
 library(ngram)
 
-#########模拟数据读入######
+#########Read the simulation data######
 data <- readRDS("/Users/kangkangyue/Desktop/scINRB/data/simulation_data/3_60%.rds")
 data_dropout <- as.matrix(data$data_dropout)
 data_true <- as.matrix(data$data_true)
@@ -16,11 +16,11 @@ data_label <- as.matrix(data[["group"]])
 impute_data <- read.csv("/Users/kangkangyue/Desktop/scINRB/imputation_knn_smoothing_data/knn_smoothing_1_60%.csv", header=FALSE)
 impute_data <- as.matrix(impute_data)
 
-########真实数据读入#######
+########Read the true data#######
 # data_label <- as.matrix(read.csv("/Users/kangkangyue/Desktop/scINRB/data/five_encode/data_label.csv",row.names=1))
 # impute_data <-  read.csv("/Users/kangkangyue/Desktop/论文/kangyue/LPLS9.28/true_data/knn_smoothing/knn_smoothing_混合数据.csv",row.names=1)
 # impute_data <- as.matrix(impute_data)
-######### kmeans聚类100次均值#############
+######### 100 averages kmeans clustering#############
 H<-t(impute_data)
 realcluster <- data_label
 ari<-vector(length=100)
@@ -45,15 +45,7 @@ print(nmi_ave)
 
 #write.csv(cluster_list,"/Users/kangkangyue/Desktop/scINRB/imputation_scINDR_data/cluster_3_60%.csv")
 
-######### tsne聚类100次均值#############
-# tSNE降维结果 
-# Rtsne输入一个表达量的表格就可以了，每一行为一个细胞，每一列为一个基因
-# dims参数设置降维之后的维度，默认值为2，
-# perplexity参数的取值必须小于(nrow(data) - 1 )/ 3;
-# theta参数取值越大，结果的准确度越低，默认值为0.5，
-# max_iter参数设置最大迭代次数。
-# pca参数表示是否对输入的原始数据进行PCA分析，然后使用PCA得到的topN主成分进行后续分析
-# t-SNE计算量特别大，对于维度较高数据，先采用PCA降维可提高运行效率，默认采用top50的主成分进行后续分析，可通过initial_dims修改这个值。
+######### 100 averages of tsne clustering#############
 tsne.coords <- Rtsne(t(impute_data),pca=FALSE,perplexity = 30,theta=0.5,check_duplicates = F)$Y
 rownames(tsne.coords) <- rownames(t(impute_data))
 colnames(tsne.coords) <- c("tSNE1","tSNE2")
@@ -81,10 +73,10 @@ print(nmi_ave)
 
 
 
-########批量模拟数据kemans聚类 单个方法
+######## simulation data kemans clustering
 source('/Users/kangkangyue/Desktop/scINRB/analysis_methods.R')
-path="/Users/kangkangyue/Desktop/scINRB" #声明test2.R所在位置
-setwd(path)  #把工作路径设置到path
+path="/Users/kangkangyue/Desktop/scINRB" 
+setwd(path)  
 method="deepimpute"
 
 for(scale_num in c(2,3,5,6)){
@@ -98,15 +90,15 @@ for(scale_num in c(2,3,5,6)){
   }
 }
 
-########批量模拟数据tsne聚类 单个方法
-rm(list=ls()) #清除当前环境中的变量
+########simulation data tsne clustering
+rm(list=ls()) 
 library(Rtsne)
 library(ggplot2)
 library(devtools)
 library(mclust)
 library(fpc)
-path="/Users/kangkangyue/Desktop/scINRB" #声明test2.R所在位置
-setwd(path)  #把工作路径设置到path
+path="/Users/kangkangyue/Desktop/scINRB" 
+setwd(path)  
 source('analysis_methods.R')
 method="deepimpute"
 
@@ -122,7 +114,7 @@ for(scale_num in c(1:5)){
   
 }
 
-########批量模拟数据pss 单个方法
+########simulation data : matrix_RMSE\matrix_psss\cell_pss\gene_pss
 
 rm(list=ls())
 library("pheatmap")
@@ -135,9 +127,9 @@ library(lattice)
 library(aplot)
 library(tidyr)
 library(Metrics)
-path="/Users/kangkangyue/Desktop/scINRB" #声明test2.R所在位置
-setwd(path)  #把工作路径设置到path
-source('methods.R')#“预装“函数
+path="/Users/kangkangyue/Desktop/scINRB" 
+setwd(path)  
+source('methods.R')
 source('analysis_methods.R')
 method="deepimpute"
 
