b462d1793340af9107ef68fc59c9d0afb6caf5b8
JGuan-lab <64457671+JGuan-lab@users.noreply.github.com>
2023-01-03 22:58:32 +0800

    Update run_demo.R

--- code/run_demo.R
+++ code/run_demo.R
@@ -1,7 +1,7 @@
 #######Read the data#############
 #simulation data
-path="/Users/kangkangyue/Desktop/scINRB/data/simulation_data" #声明test2.R所在位置
-setwd(path)  #把工作路径设置到path
+path="/Users/kangkangyue/Desktop/scINRB/data/simulation_data" 
+setwd(path)  
 data <- readRDS("4_20%.rds")
 data_sc <- as.matrix(data$data_dropout)
 data_true <- as.matrix(data$data_true)
@@ -10,10 +10,10 @@ data_bulk <- as.matrix(data$data_bulk)
 # data_sc <- read.csv("data_sc.csv",row.names=1)
 # data_bulk <- read.csv("data_bulk.csv",row.names=1)
 ###########
-path="/Users/kangkangyue/Desktop/scINRB" #声明test2.R所在位置
-setwd(path)  #把工作路径设置到path
-source('scINRB.R')#“预装“函数
-source('functions.R')#“预装“函数
+path="/Users/kangkangyue/Desktop/scINRB" 
+setwd(path)  
+source('scINRB.R')
+source('functions.R')
 library(MASS)
 r<-200
 parameter <- c(0.001,0.001,1)
